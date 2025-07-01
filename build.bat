6034527e4f469013e407be546ac2e2c16e86bd81
Voary <voary030@gmail.com>
2025-07-01 21:28:32 +0300

    okay

--- build.bat
+++ build.bat
@@ -55,4 +55,19 @@ cd ..
 
 echo WAR généré avec succès : VelonKan.war
 echo Vérifiez que application.properties est bien présent dans WEB-INF/classes
+
+
+set WAR_NAME=VelonKan.war
+set SOURCE_DIR=
+set DEST_DIR=C:\xampp\tomcat\webapps
+
+echo Copying %WAR_NAME% to %DEST_DIR% ...
+copy "%WAR_NAME%" "%DEST_DIR%\%WAR_NAME%" /Y
+
+if %ERRORLEVEL%==0 (
+    echo Deploy successful.
+) else (
+    echo Deploy failed.
+)
+
 pause
